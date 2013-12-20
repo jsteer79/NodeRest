e859f63886d519dd52a67511a0f4f2a53b105053
James Steer <jsteer79@gmail.com>
2013-12-20 12:33:14 +0000

    Add some tests, remove body parser

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var   Express = require( 'Express' )
+var   Express  = require( 'Express' )
     , NodeRest = require( './src/NodeRest.js' )
-    , app     = Express();
+    , app      = Express();
 
-app.use( Express.logger() );
-app.use( Express.bodyParser() );
-app.use( Express.static( __dirname + '/public' ) );
+app.use( Express.logger() )
+   .use( Express.json() )
+   .use( Express.urlencoded() )
+   .use( Express.static( __dirname + '/public' ) );
 NodeRest( app, 'foo' );
 NodeRest( app, 'bar' );
 app.listen( 8080 );
-
--- src/NodeRest.js
+++ src/NodeRest.js
@@ -8,27 +8,24 @@ function NodeRest( oExpress, path ) {
     this.path = path;
     this.reset();
     var oThis = this;
-    oExpress.get(    '/' + this.path , function( req, res ) { oThis.get( req, res ); } );
-    oExpress.delete( '/' + this.path , function( req, res ) { oThis.delete( req, res ); } );
-    oExpress.post(   '/' + this.path , function( req, res ) { oThis.post( req, res ); } );
-    oExpress.put(    '/' + this.path , function( req, res ) { oThis.post( req, res ); } );
+    oExpress.get(  '/' + this.path , function( req, res ) { oThis.get( req, res ); } );
+    oExpress.del(  '/' + this.path , function( req, res ) { oThis.del( req, res ); } );
+    oExpress.post( '/' + this.path , function( req, res ) { oThis.post( req, res ); } );
+    oExpress.put(  '/' + this.path , function( req, res ) { oThis.post( req, res ); } );
 }
 
 NodeRest.prototype.reset = function() {
     this.thing = 'Initial ' + this.path;
-}
+};
 
 NodeRest.prototype.get = function( req, res ) {
-    console.log( 'Get' );
     res.status( 200 );
     res.setHeader('Content-Type', 'text/plain' );
     res.setHeader('Content-Length', this.thing.length );
     res.end( this.thing );
-    console.log( 'Done' );
-}
+};
 
 NodeRest.prototype.post = function( req, res ) {
-    console.log( 'Post' );
     if( !req.param( 'thing' ) ) {
         res.status( 400 );
         res.end();
@@ -36,9 +33,9 @@ NodeRest.prototype.post = function( req, res ) {
         this.thing = req.param( 'thing' );
         this.get( req, res );
     }
-}
+};
 
-NodeRest.prototype.delete = function( req, res ) {
+NodeRest.prototype.del = function( req, res ) {
     this.reset();
     this.get( req, res );
-}
+};
